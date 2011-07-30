ede520b99c264932107bbb281422c26f3f660e7d
Tincho <devnull@localhost>
2011-07-29 21:59:57 -0300

    Makefile agregado, arrancamos con el codigo de prueba.

--- tito/SRC/tito.c
+++ tito/SRC/tito.c
@@ -1,13 +1,72 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#define F_CPU 8000000UL
 #include <util/delay.h>
 #include "tito.h"
 
 int main(void) {
-    for (;;) {
-        PORT_LED_1 = 1;
-        _delay_ms(1000);
-        PORTC = 0;
-        _delay_ms(1000);
+	
+	startup();
+/*    for (;;) {
+		if (IsBitSet(PORT_SENSOR_1, SENSOR_1_NUMBER)) {
+			MoverIzquierda();
+		} else if (IsBitSet(PORT_SENSOR_2, SENSOR_2_NUMBER)) {
+			MoverDerecha();
+		} else {
+			MoverAdelante();
+		}
     }
+*/
+	for(;;){
+		MoverAdelante();
+	}
+}
+
+void MoverIzquierda (void) {
+	SetBit (PORT_IN_1_A, IN_1_A_NUMBER);
+	ClearBit (PORT_IN_2_A, IN_2_A_NUMBER);
+	ClearBit (PORT_IN_3_B, IN_3_B_NUMBER);
+	ClearBit (PORT_IN_4_B, IN_4_B_NUMBER);
+}	
+void MoverDerecha (void) {
+	ClearBit (PORT_IN_1_A, IN_1_A_NUMBER);
+	ClearBit (PORT_IN_2_A, IN_2_A_NUMBER);
+	ClearBit (PORT_IN_3_B, IN_3_B_NUMBER);
+	SetBit (PORT_IN_4_B, IN_4_B_NUMBER);
+}	
+void MoverAdelante (void) {
+	SetBit (PORT_IN_1_A, IN_1_A_NUMBER);
+	SetBit (PORT_IN_2_A, IN_2_A_NUMBER);
+	SetBit (PORT_IN_3_B, IN_3_B_NUMBER);
+	SetBit (PORT_IN_4_B, IN_4_B_NUMBER);
+}	
+
+
+void startup (void){
+
+	SetBit (DDR_LED_1, LED_1_NUMBER);
+	SetBit (DDR_LED_2, LED_2_NUMBER);
+	SetBit (DDR_LED_3, LED_3_NUMBER);
+	SetBit (DDR_LED_4, LED_4_NUMBER);
+
+	SetBit (DDR_ENABLE_A, ENABLE_A_NUMBER);
+	SetBit (DDR_ENABLE_B, ENABLE_B_NUMBER);
+
+	SetBit (DDR_IN_1_A, IN_1_A_NUMBER);
+	SetBit (DDR_IN_2_A, IN_2_A_NUMBER);
+	SetBit (DDR_IN_3_B, IN_3_B_NUMBER);
+	SetBit (DDR_IN_4_B, IN_4_B_NUMBER);
+
+	ClearBit (DDR_SENSOR_1, SENSOR_1_NUMBER);
+	ClearBit (DDR_SENSOR_2, SENSOR_2_NUMBER);
+
+	// habilita prender los motores
+	SetBit (PORT_ENABLE_A, ENABLE_A_NUMBER);
+	SetBit (PORT_ENABLE_B, ENABLE_B_NUMBER);
+
+/*	DDRC |= ((1<<PC0) | (1<<PC1) | (1<<PC2));
+	DDRC &= ~((1<<PC3) | (1<<PC4) | (1<<PC5));
+	DDRD &=~ 1<<PD5;
+	PORTD |= 1<<PD5;
+*/
 }
--- tito/SRC/tito.h
+++ tito/SRC/tito.h
@@ -1,6 +1,11 @@
 #ifndef HEADER_TITO
 #define HEADER_TITO
 
+void MoverIzquierda (void);
+void MoverDerecha (void);
+void MoverAdelante (void);
+void startup (void);
+
 #define PORT_LED_1_NAME  	C
 #define LED_1_NUMBER     	3
 #define PORT_LED_1    def_port_reg(PORT_LED_1_NAME)
@@ -25,6 +30,7 @@
 #define PIN_LED_4      def_pin_reg(PORT_LED_4_NAME)
 #define DDR_LED_4      def_ddr_reg(PORT_LED_4_NAME)
 
+
 #define PORT_SENSOR_1_NAME  	D
 #define SENSOR_1_NUMBER     	5
 #define PORT_SENSOR_1    def_port_reg(PORT_SENSOR_1_NAME)
@@ -44,9 +50,47 @@
 #define DDR_SENSOR_3      def_ddr_reg(PORT_SENSOR_3_NAME)
 
 
+#define PORT_ENABLE_A_NAME  	B
+#define ENABLE_A_NUMBER     	1
+#define PORT_ENABLE_A    def_port_reg(PORT_ENABLE_A_NAME)
+#define PIN_ENABLE_A      def_pin_reg(PORT_ENABLE_A_NAME)
+#define DDR_ENABLE_A      def_ddr_reg(PORT_ENABLE_A_NAME)
+
+#define PORT_ENABLE_B_NAME  	B
+#define ENABLE_B_NUMBER     	2
+#define PORT_ENABLE_B    def_port_reg(PORT_ENABLE_B_NAME)
+#define PIN_ENABLE_B      def_pin_reg(PORT_ENABLE_B_NAME)
+#define DDR_ENABLE_B      def_ddr_reg(PORT_ENABLE_B_NAME)
+
+
+#define PORT_IN_1_A_NAME	D
+#define IN_1_A_NUMBER		1
+#define PORT_IN_1_A    		def_port_reg(PORT_IN_1_A_NAME)
+#define PIN_IN_1_A			def_pin_reg(PORT_IN_1_A_NAME)
+#define DDR_IN_1_A			def_ddr_reg(PORT_IN_1_A_NAME)
+
+#define PORT_IN_2_A_NAME	D
+#define IN_2_A_NUMBER		0
+#define PORT_IN_2_A    		def_port_reg(PORT_IN_2_A_NAME)
+#define PIN_IN_2_A			def_pin_reg(PORT_IN_2_A_NAME)
+#define DDR_IN_2_A			def_ddr_reg(PORT_IN_2_A_NAME)
+
+#define PORT_IN_3_B_NAME	C
+#define IN_3_B_NUMBER		4
+#define PORT_IN_3_B    		def_port_reg(PORT_IN_3_B_NAME)
+#define PIN_IN_3_B			def_pin_reg(PORT_IN_3_B_NAME)
+#define DDR_IN_3_B			def_ddr_reg(PORT_IN_3_B_NAME)
+
+#define PORT_IN_4_B_NAME	C
+#define IN_4_B_NUMBER		5
+#define PORT_IN_4_B    		def_port_reg(PORT_IN_4_B_NAME)
+#define PIN_IN_4_B			def_pin_reg(PORT_IN_4_B_NAME)
+#define DDR_IN_4_B			def_ddr_reg(PORT_IN_4_B_NAME)
+
+
 #define SetBit(Byte,Bit)	 (Byte |= (1<<Bit))
 #define	ClearBit(Byte,Bit)	 (Byte &= (~(1<<Bit)))
-#define IsBitSet(Byte,Bit)	 ( (Byte & (1<<Bit)) ? True : False )
+#define IsBitSet(Byte,Bit)	 ( (Byte & (1<<Bit)) ? 1 : 0 )
 
 #define concat(a,b)		a ## b
 #define def_port_reg(name)	concat(PORT,name)
