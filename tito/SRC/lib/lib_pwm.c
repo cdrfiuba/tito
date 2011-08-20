2a5c2d0ffd2ecd15e541c691885d3c3901695b8c
Martin Mello Teggia <devnull@localhost>
2011-08-19 22:14:37 -0300

    empezamos de nuevo el codigo

---
+++ tito/SRC/lib/lib_pwm.c
@@ -0,0 +1,109 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define F_CPU 8000000UL
+#include "lib_pwm.h"
+
+void pwm_config(void){
+	// Configuramos los puertos como salidas en cero
+	DDR_PWM1 |= _BV(PIN_NUM_PWM1);
+	DDR_PWM2 |= _BV(PIN_NUM_PWM2);
+
+	PORT_PWM1 &=~ _BV(PIN_NUM_PWM1);
+	PORT_PWM2 &=~ _BV(PIN_NUM_PWM2);
+
+	// pines de sentido de giro
+	PORT_MOT1_S1 &=~ _BV(PORT_NUM_MOT1_S1); 	
+	PORT_MOT1_S2 &=~ _BV(PORT_NUM_MOT1_S2); 	
+	PORT_MOT2_S1 &=~ _BV(PORT_NUM_MOT2_S1); 	
+	PORT_MOT2_S2 &=~ _BV(PORT_NUM_MOT2_S2); 	
+	 	
+	DDR_MOT1_S1 |= _BV(PORT_NUM_MOT1_S1); 	
+	DDR_MOT1_S2 |= _BV(PORT_NUM_MOT1_S2); 	
+	DDR_MOT2_S1 |= _BV(PORT_NUM_MOT2_S1); 	
+	DDR_MOT2_S2 |= _BV(PORT_NUM_MOT2_S2);
+	
+	// Configuramos el PWM
+	TCCR1A = PWM_TCCR1A;
+
+	TCCR1B = PWM_TCCR1B; 	
+	ICR1 = PWM_ICR1; 
+
+	OCR1A = 0;
+	OCR1B = 0;
+
+	// Activamos la IRQ de OVF
+	TIMSK |= _BV(TOIE1) ;
+
+	// Apaga los motores
+	mot1_sent(LIBRE);
+	mot2_sent(LIBRE);
+}
+
+/*inline void set_vel_motor_1( uint8_t velocidad ){
+	vel_motor_1 = velocidad*PWM_ICR1/255;
+}   DEFINIDO EN EL .H!!!*/
+
+/*inline void set_vel_motor_2( uint16_t velocidad ){
+	vel_motor_2 = velocidad ;
+}   DEFINIDO EN EL .H!!!*/
+
+// Sentido de giro del motor 1
+// LIBRE, AD, AT, DET
+inline void mot1_sent(sentido_t sentido){
+	switch(sentido){
+		case LIBRE:
+			PORT_MOT1_S1 &=~ _BV(PORT_NUM_MOT1_S1);
+			PORT_MOT1_S2 &=~ _BV(PORT_NUM_MOT1_S2);
+			break;
+		case AT:
+			PORT_MOT1_S1 |= _BV(PORT_NUM_MOT1_S1);
+			PORT_MOT1_S2 &=~ _BV(PORT_NUM_MOT1_S2);
+			break;
+		case AD:
+			PORT_MOT1_S1 &=~ _BV(PORT_NUM_MOT1_S1);
+			PORT_MOT1_S2 |= _BV(PORT_NUM_MOT1_S2);
+			break;
+		case DET:
+			PORT_MOT1_S1 |= _BV(PORT_NUM_MOT1_S1);
+			PORT_MOT1_S2 |= _BV(PORT_NUM_MOT1_S2);
+			break;
+	}
+}
+
+// Sentido de giro del motor 2
+// LIBRE, AD, AT, DET
+inline void mot2_sent(sentido_t sentido){
+	switch(sentido){
+		case LIBRE:
+			PORT_MOT2_S1 &=~ _BV(PORT_NUM_MOT2_S1);
+			PORT_MOT2_S2 &=~ _BV(PORT_NUM_MOT2_S2);
+			break;
+		case AT:
+			PORT_MOT2_S1 |= _BV(PORT_NUM_MOT2_S1);
+			PORT_MOT2_S2 &=~ _BV(PORT_NUM_MOT2_S2);
+			break;
+		case AD:
+			PORT_MOT2_S1 &=~ _BV(PORT_NUM_MOT2_S1);
+			PORT_MOT2_S2 |= _BV(PORT_NUM_MOT2_S2);
+			break;
+		case DET:
+			PORT_MOT2_S1 |= _BV(PORT_NUM_MOT2_S1);
+			PORT_MOT2_S2 |= _BV(PORT_NUM_MOT2_S2);
+			break;
+	}
+}
+
+void pwm_start(void) {
+	TCCR1B |= PRESCALER_PWM_ON;
+}
+
+void pwm_stop(void) {
+	TCCR1B &=~ PRESCALER_PWM_OFF;
+}
+
+ISR( TIMER1_OVF_vect ) { 
+	OCR1A = vel_motor_1 ;
+	OCR1B = vel_motor_2 ;
+}
+
--- tito/SRC/tito.h
+++ tito/SRC/tito.h
@@ -1,9 +1,7 @@
 #ifndef HEADER_TITO
 #define HEADER_TITO
 
-void MoverIzquierda (void);
-void MoverDerecha (void);
-void MoverAdelante (void);
+
 void startup (void);
 
 #define PORT_LED_1_NAME  	C
@@ -30,7 +28,6 @@ void startup (void);
 #define PIN_LED_4      def_pin_reg(PORT_LED_4_NAME)
 #define DDR_LED_4      def_ddr_reg(PORT_LED_4_NAME)
 
-
 #define PORT_SENSOR_1_NAME  	D
 #define SENSOR_1_NUMBER     	5
 #define PORT_SENSOR_1    def_port_reg(PORT_SENSOR_1_NAME)
@@ -49,52 +46,19 @@ void startup (void);
 #define PIN_SENSOR_3      def_pin_reg(PORT_SENSOR_3_NAME)
 #define DDR_SENSOR_3      def_ddr_reg(PORT_SENSOR_3_NAME)
 
-
-#define PORT_ENABLE_A_NAME  	B
-#define ENABLE_A_NUMBER     	1
-#define PORT_ENABLE_A    def_port_reg(PORT_ENABLE_A_NAME)
-#define PIN_ENABLE_A      def_pin_reg(PORT_ENABLE_A_NAME)
-#define DDR_ENABLE_A      def_ddr_reg(PORT_ENABLE_A_NAME)
-
-#define PORT_ENABLE_B_NAME  	B
-#define ENABLE_B_NUMBER     	2
-#define PORT_ENABLE_B    def_port_reg(PORT_ENABLE_B_NAME)
-#define PIN_ENABLE_B      def_pin_reg(PORT_ENABLE_B_NAME)
-#define DDR_ENABLE_B      def_ddr_reg(PORT_ENABLE_B_NAME)
-
-
-#define PORT_IN_1_A_NAME	D
-#define IN_1_A_NUMBER		1
-#define PORT_IN_1_A    		def_port_reg(PORT_IN_1_A_NAME)
-#define PIN_IN_1_A			def_pin_reg(PORT_IN_1_A_NAME)
-#define DDR_IN_1_A			def_ddr_reg(PORT_IN_1_A_NAME)
-
-#define PORT_IN_2_A_NAME	D
-#define IN_2_A_NUMBER		0
-#define PORT_IN_2_A    		def_port_reg(PORT_IN_2_A_NAME)
-#define PIN_IN_2_A			def_pin_reg(PORT_IN_2_A_NAME)
-#define DDR_IN_2_A			def_ddr_reg(PORT_IN_2_A_NAME)
-
-#define PORT_IN_3_B_NAME	C
-#define IN_3_B_NUMBER		4
-#define PORT_IN_3_B    		def_port_reg(PORT_IN_3_B_NAME)
-#define PIN_IN_3_B			def_pin_reg(PORT_IN_3_B_NAME)
-#define DDR_IN_3_B			def_ddr_reg(PORT_IN_3_B_NAME)
-
-#define PORT_IN_4_B_NAME	C
-#define IN_4_B_NUMBER		5
-#define PORT_IN_4_B    		def_port_reg(PORT_IN_4_B_NAME)
-#define PIN_IN_4_B			def_pin_reg(PORT_IN_4_B_NAME)
-#define DDR_IN_4_B			def_ddr_reg(PORT_IN_4_B_NAME)
-
+#define PORT_SENSOR_CURVA_NAME  	D
+#define SENSOR_CURVA_NUMBER     	2
+#define PORT_SENSOR_CURVA    def_port_reg(PORT_SENSOR_CURVA_NAME)
+#define PIN_SENSOR_CURVA      def_pin_reg(PORT_SENSOR_CURVA_NAME)
+#define DDR_SENSOR_CURVA      def_ddr_reg(PORT_SENSOR_CURVA_NAME)
 
 #define SetBit(Byte,Bit)	 (Byte |= (1<<Bit))
 #define	ClearBit(Byte,Bit)	 (Byte &= (~(1<<Bit)))
 #define IsBitSet(Byte,Bit)	 ( (Byte & (1<<Bit)) ? 1 : 0 )
-
+/*
 #define concat(a,b)		a ## b
 #define def_port_reg(name)	concat(PORT,name)
 #define def_pin_reg(name)	concat(PIN,name)
 #define def_ddr_reg(name)	concat(DDR,name)
-
+*/
 #endif
