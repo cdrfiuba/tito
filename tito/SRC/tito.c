a2124a1f60b9625a36dd9a43472b16bc97b654da
damian <devnull@localhost>
2011-11-10 22:12:12 -0300

    Cambios y pruebas para que la fsm ande en un micro AVR

--- tito/SRC/tito.c
+++ tito/SRC/tito.c
@@ -4,51 +4,284 @@
 #include <util/delay.h>
 #include "tito.h"
 #include "lib/lib_pwm.h"
+#include <avr/pgmspace.h>
 
-int main(void) {
 
-	// llamo a inicializar, puertos y PWM
-	startup();
+/**
+ estructura para transiciones:
+ determina el flujo de la fsm, es decir,
+ qué eventos están permitidos para qué estados,
+ y cuál es la función encargada de manejar ese estado.
+*/
+typedef struct {
+    uint16_t st;
+    uint16_t ev;
+    uint16_t st_new;//(*fn)(void *);
+} tTransition;
 
-	// establezco la dirección de los motores
-	mot1_sent(AD);
-	mot2_sent(AD);
+/**
+ handlers de eventos
+ */
+uint16_t manejar_estado(uint16_t estado) {
+	#define FACTOR 1 // coeficiente de disminuición de velocidad
+	switch (estado) {
+		case ST_EN_LINEA:
+			PWM1_VEL(100 * FACTOR);
+			PWM2_VEL(100 * FACTOR);
+			mot1_sent(AD);
+			mot2_sent(AD);
+			break;
+	
 
-	// bucle principal,
-	// hago polling del estado de los sensores,
-	// (en un orden específico)
-	// y actúo en consecuencia
-	for(;;)	{
-		// si están los sensores de la izquierda y de la derecha,
-		// entonces asumo que estoy cruzando una línea horizontal,
-		// por lo que no cambio nada
-		if (SENSOR_IZQUIERDA && SENSOR_DERECHA) {
-			// no hacer nada
-		// si está el sensor de la izquierda,
-		// entonces asumo que estoy desviado hacia la derecha,
-		// por lo que debo girar hacia la izquierda
-		} else if (SENSOR_IZQUIERDA) {
-			PWM1_VEL(0);
-			PWM2_VEL(100);
-		// si está el sensor de la derecha,
-		// entonces asumo que estoy desviado hacia la izquierda,
-		// por lo que debo girar hacia la derecha
-		} else if (SENSOR_DERECHA) {
-			PWM1_VEL(100);
-			PWM2_VEL(0);
-		// si está el sensor del medio,
-		// entonces asumo que estoy yendo bien,
-		// por lo que avanzo hacia adelante
-		} else if (SENSOR_MEDIO) {
-			PWM1_VEL(100);
-			PWM2_VEL(100);
-		}
+		case ST_YENDOSE_POCO_POR_DERECHA:
+			PWM1_VEL(75 * FACTOR);
+			PWM2_VEL(100 * FACTOR);
+			mot1_sent(AT);
+			mot2_sent(AT);
+			break;
+	
+		case ST_YENDOSE_MUCHO_POR_DERECHA:
+			PWM1_VEL(35 * FACTOR);
+			PWM2_VEL(100 * FACTOR);
+			mot1_sent(AT);
+			mot2_sent(AT);
+			break;
+	
+		case ST_AFUERA_POR_DERECHA:
+			PWM1_VEL(0 * FACTOR);
+			PWM2_VEL(100 * FACTOR);
+			mot1_sent(AT);
+			mot2_sent(AT);
+			break;
+	
+		case ST_VOLVIENDO_POR_DERECHA:
+			PWM1_VEL(100 * FACTOR);
+			PWM2_VEL(85 * FACTOR);
+			mot1_sent(AT);
+			mot2_sent(AT);
+			break;
+	
+		case ST_VOLVIO_POR_DERECHA:
+			PWM1_VEL(100 * FACTOR);
+			PWM2_VEL(95 * FACTOR);
+			mot1_sent(AT);
+			mot2_sent(AT);
+			break;
+	
+
+		case ST_YENDOSE_POCO_POR_IZQUIERDA:
+			PWM1_VEL(100 * FACTOR);
+			PWM2_VEL(75 * FACTOR);
+			mot1_sent(AT);
+			mot2_sent(AT);
+			break;
+	
+		case ST_YENDOSE_MUCHO_POR_IZQUIERDA:
+			PWM1_VEL(100 * FACTOR);
+			PWM2_VEL(35 * FACTOR);
+			mot1_sent(AT);
+			mot2_sent(AT);
+			break;
+	
+		case ST_AFUERA_POR_IZQUIERDA:
+			PWM1_VEL(100 * FACTOR);
+			PWM2_VEL(0 * FACTOR);
+			mot1_sent(AT);
+			mot2_sent(AT);
+			break;
+	
+		case ST_VOLVIENDO_POR_IZQUIERDA:
+			PWM1_VEL(85 * FACTOR);
+			PWM2_VEL(100 * FACTOR);
+			mot1_sent(AT);
+			mot2_sent(AT);
+			break;
+	
+		case ST_VOLVIO_POR_IZQUIERDA:
+			PWM1_VEL(95 * FACTOR);
+			PWM2_VEL(100 * FACTOR);
+			mot1_sent(AT);
+			mot2_sent(AT);
+			break;
+
+			// esto permite que cuando no existe una transición
+			// explícita, el estado no cambie
+			
 	}
-
-
+	return estado;
 }
 
+/**
+ Función principal para la fsm, se encarga de devolver
+ el evento que corresponde.
+ Si se quisiera implementar una cola de eventos,
+ esta función sería la responsable de manejarla.
+*/
+uint16_t obtener_evento(void) {
+	return ((SENSOR_DERECHA << 2) | (SENSOR_CENTRO << 1) | (SENSOR_IZQUIERDA));
+}
 
+/**
+ función principal
+*/
+int main() {
+
+	uint16_t i = 0;
+
+	// declaro "state" y "event" en vez de "estado" y "evento"
+	// porque se distinguen más entre sí
+	uint16_t state = ST_EN_LINEA;
+	uint16_t event = EV_CUALQUIERA;
+
+	// acá se definen las transiciones de la fsm:
+	// esencialmente esto ES la fsm
+	static tTransition trans[] PROGMEM = {
+		{ST_EN_LINEA, EV_SENSORES_BNN, ST_YENDOSE_MUCHO_POR_DERECHA},
+		{ST_EN_LINEA, EV_SENSORES_BBN, ST_YENDOSE_POCO_POR_DERECHA},
+		{ST_EN_LINEA, EV_SENSORES_NNB, ST_YENDOSE_MUCHO_POR_IZQUIERDA},
+		{ST_EN_LINEA, EV_SENSORES_NBB, ST_YENDOSE_POCO_POR_IZQUIERDA},
+
+		{ST_YENDOSE_POCO_POR_DERECHA, EV_SENSORES_NBN, ST_EN_LINEA},
+		{ST_YENDOSE_POCO_POR_DERECHA, EV_SENSORES_BNN, ST_YENDOSE_MUCHO_POR_DERECHA},
+		{ST_YENDOSE_MUCHO_POR_DERECHA, EV_SENSORES_BBN, ST_VOLVIO_POR_DERECHA},
+		{ST_YENDOSE_MUCHO_POR_DERECHA, EV_SENSORES_NNN, ST_AFUERA_POR_DERECHA},
+		{ST_AFUERA_POR_DERECHA, EV_SENSORES_BNN, ST_VOLVIENDO_POR_DERECHA},
+		{ST_VOLVIENDO_POR_DERECHA, EV_SENSORES_NBN, ST_EN_LINEA},
+		{ST_VOLVIENDO_POR_DERECHA, EV_SENSORES_BBN, ST_VOLVIO_POR_DERECHA},
+		{ST_VOLVIENDO_POR_DERECHA, EV_SENSORES_NNN, ST_AFUERA_POR_DERECHA},
+		{ST_VOLVIO_POR_DERECHA, EV_SENSORES_NBN, ST_EN_LINEA},
+		{ST_VOLVIO_POR_DERECHA, EV_SENSORES_BNN, ST_YENDOSE_MUCHO_POR_DERECHA},
+
+		{ST_YENDOSE_POCO_POR_IZQUIERDA, EV_SENSORES_NBN, ST_EN_LINEA},
+		{ST_YENDOSE_POCO_POR_IZQUIERDA, EV_SENSORES_NNB, ST_YENDOSE_MUCHO_POR_IZQUIERDA},
+		{ST_YENDOSE_MUCHO_POR_IZQUIERDA, EV_SENSORES_NBB, ST_VOLVIO_POR_IZQUIERDA},
+		{ST_YENDOSE_MUCHO_POR_IZQUIERDA, EV_SENSORES_NNN, ST_AFUERA_POR_IZQUIERDA},
+		{ST_AFUERA_POR_IZQUIERDA, EV_SENSORES_NNB, ST_VOLVIENDO_POR_IZQUIERDA},
+		{ST_VOLVIENDO_POR_IZQUIERDA, EV_SENSORES_NBN, ST_EN_LINEA},
+		{ST_VOLVIENDO_POR_IZQUIERDA, EV_SENSORES_NBB, ST_VOLVIO_POR_IZQUIERDA},
+		{ST_VOLVIENDO_POR_IZQUIERDA, EV_SENSORES_NNN, ST_AFUERA_POR_IZQUIERDA},
+		{ST_VOLVIO_POR_IZQUIERDA, EV_SENSORES_NBN, ST_EN_LINEA},
+		{ST_VOLVIO_POR_IZQUIERDA, EV_SENSORES_NNB, ST_YENDOSE_MUCHO_POR_IZQUIERDA},
+
+		{ST_CUALQUIERA, EV_CUALQUIERA, ST_CUALQUIERA} // transición default
+	};
+	#define TRANS_COUNT 50 //(sizeof(trans)/sizeof(*trans))
+
+	startup();
+
+	// ciclo principal del programa
+	while (1) {
+
+		/*while (1) {
+			PWM1_VEL(100);
+			PWM2_VEL(100);
+		}*/
+
+        PWM1_VEL(0);
+        PWM2_VEL(0);
+
+		// ciclos para esperar a que arranque cuando
+		// se suelta el botón
+		while (BOTON_NO_APRETADO);
+		_delay_ms(50); //rebote botón
+		while (BOTON_APRETADO);
+		_delay_ms(5); //rebote botón
+
+		state = ST_EN_LINEA;
+		PWM1_VEL(100);
+		PWM2_VEL(100);
+		_delay_ms(1500);
+		// Ciclo de la fsm
+		while (BOTON_NO_APRETADO) {// si se quiere que la fsm termine, se usa (state != ST_FIN)
+			event = obtener_evento();
+			if (event == 0){
+				mot1_sent(AD);
+				mot2_sent(AD);			
+				PWM1_VEL(100);			
+				PWM2_VEL(100);
+			}
+			if (event == 0){
+				mot1_sent(AD);
+				mot2_sent(AD);			
+				PWM1_VEL(100);			
+				PWM2_VEL(100);
+			}
+			if (event == 1){
+				mot1_sent(AD);
+				mot2_sent(AD);			
+				PWM1_VEL(100);			
+				PWM2_VEL(0);
+			}
+			if (event == 2){
+				mot1_sent(AD);
+				mot2_sent(AD);			
+				PWM1_VEL(0);			
+				PWM2_VEL(100);
+			}
+			if (event == 3){
+				mot1_sent(AD);
+				mot2_sent(AD);			
+				PWM1_VEL(0);			
+				PWM2_VEL(0);
+			}
+			if (event == 4){
+				mot1_sent(AD);
+				mot2_sent(AT);			
+				PWM1_VEL(100);			
+				PWM2_VEL(100);
+			}
+			if (event == 5){
+				mot1_sent(AD);
+				mot2_sent(AT);			
+				PWM1_VEL(100);			
+				PWM2_VEL(0);
+			}
+			if (event == 6){
+				mot1_sent(AD);
+				mot2_sent(AT);			
+				PWM1_VEL(0);			
+				PWM2_VEL(100);
+			}
+			if (event == 7){
+				mot1_sent(AT);
+				mot2_sent(AT);			
+				PWM1_VEL(100);			
+				PWM2_VEL(100);
+			}
+
+/*			event = obtener_evento(); // obtiene el evento a procesar
+			// cicla por todas las transiciones, y busca la que coincida con el
+			// estado y evento actuales
+			for (i = 0; i < TRANS_COUNT; i++) {
+				if ((state == pgm_read_byte_near(&(trans[i].st))) || (ST_CUALQUIERA == pgm_read_byte_near(&(trans[i].st)))) {
+					if ((event == pgm_read_byte_near(&(trans[i].ev))) || (EV_CUALQUIERA == pgm_read_byte_near(&(trans[i].ev)))) {
+						// llama a la función encargada de 
+						// manejar el estado actual y devolver el nuevo estado
+						state = manejar_estado(pgm_read_byte_near(&(trans[i].st_new)));
+						break;
+					}
+				}
+			}
+			if (i == TRANS_COUNT) {
+				PWM1_VEL(0);
+				PWM2_VEL(0);
+				_delay_ms(500);
+			}
+*/
+		}
+
+		// fin de tareas, para poder empezar de nuevo
+        PWM1_VEL(0);
+        PWM2_VEL(0);
+		_delay_ms(50); //rebote botón
+
+        while (BOTON_APRETADO);
+		_delay_ms(50); //rebote botón
+
+	}
+
+	return 1;
+}
 
 void startup (void) {
 // setear puertos de lectura o escritura,
@@ -66,6 +299,11 @@ void startup (void) {
 	ClearBit (DDR_SENSOR_3, SENSOR_3_NUMBER);
 	ClearBit (DDR_SENSOR_CURVA, SENSOR_CURVA_NUMBER);
 
+	// botón
+	ClearBit (DDR_BOTON, BOTON_NUMBER);
+	SetBit (PORT_BOTON, BOTON_NUMBER);
+
+
 	// PWM
 	pwm_config();
 	pwm_start();
@@ -73,4 +311,9 @@ void startup (void) {
 	// pone el flag global para activar interrupciones
 	sei();
 
+	// establezco la dirección de los motores
+	mot1_sent(AD);
+	mot2_sent(AD);
+
 }
+
--- tito/SRC/tito.h
+++ tito/SRC/tito.h
@@ -1,8 +1,44 @@
 #ifndef HEADER_TITO
 #define HEADER_TITO
-
-
+/**
+ Prototipos
+*/
 void startup (void);
+uint16_t obtener_evento(void);
+
+/**
+ estados y eventos para la fsm
+*/
+typedef enum estados {
+	ST_CUALQUIERA,
+
+	ST_EN_LINEA,
+
+	ST_YENDOSE_POCO_POR_DERECHA,
+	ST_YENDOSE_MUCHO_POR_DERECHA,
+	ST_AFUERA_POR_DERECHA,
+	ST_VOLVIENDO_POR_DERECHA,
+	ST_VOLVIO_POR_DERECHA,
+
+	ST_YENDOSE_POCO_POR_IZQUIERDA,
+	ST_YENDOSE_MUCHO_POR_IZQUIERDA,
+	ST_AFUERA_POR_IZQUIERDA,
+	ST_VOLVIENDO_POR_IZQUIERDA,
+	ST_VOLVIO_POR_IZQUIERDA,
+	ST_ERROR
+} estados_t;
+typedef enum eventos {
+	EV_CUALQUIERA = 8,
+
+	EV_SENSORES_NNN = 0,
+	EV_SENSORES_NNB = 1,
+	EV_SENSORES_NBN = 2,
+	EV_SENSORES_NBB = 3,
+	EV_SENSORES_BNN = 4,
+	EV_SENSORES_BNB = 5,
+	EV_SENSORES_BBN = 6,
+	EV_SENSORES_BBB = 7
+} eventos_t;
 
 #define PORT_LED_1_NAME  	C
 #define LED_1_NUMBER     	3
@@ -52,13 +88,24 @@ void startup (void);
 #define PIN_SENSOR_CURVA      def_pin_reg(PORT_SENSOR_CURVA_NAME)
 #define DDR_SENSOR_CURVA      def_ddr_reg(PORT_SENSOR_CURVA_NAME)
 
+#define PORT_BOTON_NAME  	B
+#define BOTON_NUMBER     	0
+#define PORT_BOTON       def_port_reg(PORT_BOTON_NAME)
+#define PIN_BOTON         def_pin_reg(PORT_BOTON_NAME)
+#define DDR_BOTON  	      def_ddr_reg(PORT_BOTON_NAME)
+
 #define SetBit(Byte,Bit)	 (Byte |= (1<<Bit))
 #define	ClearBit(Byte,Bit)	 (Byte &= (~(1<<Bit)))
-#define IsBitSet(Byte,Bit)	 ( (Byte & (1<<Bit)) ? 1 : 0 )
+#define IsBitSet(Byte,Bit)	 ((Byte>>Bit) & 1)
 
 #define SENSOR_IZQUIERDA	IsBitSet(PIN_SENSOR_1, SENSOR_1_NUMBER)
-#define SENSOR_MEDIO		IsBitSet(PIN_SENSOR_2, SENSOR_2_NUMBER)
+#define SENSOR_CENTRO		IsBitSet(PIN_SENSOR_2, SENSOR_2_NUMBER)
 #define SENSOR_DERECHA		IsBitSet(PIN_SENSOR_3, SENSOR_3_NUMBER)
+#define SENSOR_CURVA		IsBitSet(PIN_SENSOR_CURVA, SENSOR_CURVA_NUMBER)
+
+#define BOTON_APRETADO 		(!(PINB&(1<<PB0)))
+#define BOTON_NO_APRETADO 	(PINB&(1<<PB0))
+
 /*
 #define concat(a,b)		a ## b
 #define def_port_reg(name)	concat(PORT,name)
